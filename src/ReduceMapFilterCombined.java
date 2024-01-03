e001face80cd9892ad2ba97528c060dbd2e62acc
dch2kor <ChandraSekar.DineshKumar@in.bosch.com>
2024-01-03 13:11:22 +0530

    reduce,map, filter combined example min max

--- src/ReduceMapFilterCombined.java
+++ src/ReduceMapFilterCombined.java
@@ -11,10 +11,29 @@ public class ReduceMapFilterCombined {
                .filter(students -> students.getGpa() >=3.8)
                .map(Student::getNoteBooks)
                .reduce(0, Integer::sum);
+                //   .reduce(0, Integer::max);
+                //   .reduce(0, Integer::min);
+                // identity is the default value
+    }
 
+    public static Optional<Integer> getStudentsWithNotebooksMax(List<Student> studentList){
+        return studentList
+                .stream()
+                .filter(students -> students.getGpa() >=3.8)
+                .map(Student::getNoteBooks)
+                .reduce(Integer::max);
+        //   .reduce(0, Integer::max);
+        //   .reduce(0, Integer::min);
+        // identity is the default value
     }
     public static void main(String[] args) {
         // get the students with the total no of notebooks
         System.out.println(getStudentsWithTotalNotebooks(StudentDataBase.getAllStudents()));
+        Optional<Integer> maxValue= getStudentsWithNotebooksMax(StudentDataBase.getAllStudents());
+        if(maxValue.isPresent()){
+            System.out.println(maxValue.get());
+        }else{
+            System.out.println("List is empty");
+        }
     }
 }
